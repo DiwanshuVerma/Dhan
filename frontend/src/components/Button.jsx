efcfc3a42753acf762f61be7d3d34e12eeb026a1
diwanshu verma <diwanshu63019@gmail.com>
2024-11-11 17:09:13 +0530

    added active property to btn

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,7 @@
 export const Button = ({label, onClick}) => {
     return (
         <div className="w-full text-center mt-4">
-            <button onClick={onClick} className="w-1/2 bg-[#75238a] text-xl  p-2 rounded-full">
+            <button onClick={onClick} className="w-1/2 bg-[#75238a] active:bg-[#b54ecf] text-xl  p-2 rounded-full">
                 {label}
             </button>
         </div>
