1d75f463fc393ccd08d8efd25c6fd18418c3eae3
diwanshu verma <diwanshu63019@gmail.com>
2024-11-11 17:12:00 +0530

    new logout feature

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react'
 import { Appbar } from '../components/Appbar'
 import { Users } from '../components/Users'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 export const Dashboard = () => {
     return (
-        <div className="md:px-10 sm:px-6 px-3 py-10 text-white min-w-screen min-h-screen bg-slate-950 overflow-hidden z-10 flex flex-col gap-4">
+        <div className="md:px-10 sm:px-6 px-0 py-10 text-white min-w-screen min-h-screen bg-slate-950 overflow-hidden z-10 flex flex-col gap-4">
             <Appbar/>
             <Balance />
             <Users />
@@ -18,6 +19,7 @@ export const Dashboard = () => {
 const Balance = () => {
     const [balance, setBalance] = useState(0)
     const token = localStorage.getItem('token')
+    const navigate = useNavigate()
 
     useEffect(() => {
 
@@ -36,8 +38,15 @@ const Balance = () => {
             fetchBalance()
         }, [])
     return (
-        <div className="text-2xl p-4">
+        <div className="text-2xl items-center p-4 flex justify-between">
             <div>Your Balance <span className='text-green-500 font-semibold'>${balance.toFixed(2)}</span></div>
+            <div onClick={() => {
+                localStorage.removeItem('token')
+                navigate('/signin')
+            }} className=' text-lg flex items-center rounded-md px-2 bg-red-700 cursor-pointer'>
+                <p className='sm:block hidden'>Log out</p>
+                <img className='sm:w-10 w-8' src='logout.svg'/>
+                </div>
         </div>
     )
 }
