5676c4dd2859817b9e70da49746676198be112d5
diwanshu <diwanshu63019@gmail.com>
2025-02-04 10:22:50 +0530

    log out prompt

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom'
 export const Dashboard = () => {
     return (
         <div className="md:px-10 sm:px-6 px-0 py-10 text-white min-w-screen min-h-screen bg-slate-950 overflow-hidden z-10 flex flex-col gap-4">
-            <Appbar/>
+            <Appbar />
             <Balance />
             <Users />
         </div>
@@ -35,18 +35,24 @@ const Balance = () => {
                 console.log('Error while fetching balance: ', error)
             }
         }
-            fetchBalance()
-        }, [])
+        fetchBalance()
+    }, [])
+
+    const handleLogOut = () => {
+       if(window.confirm("Do you really want to log out?")) {
+            localStorage.removeItem('token')
+            navigate('/signin')
+        }
+
+    }
     return (
         <div className="text-2xl items-center p-4 flex justify-between">
             <div>Your Balance <span className='text-green-500 font-semibold'>${balance.toFixed(2)}</span></div>
-            <div onClick={() => {
-                localStorage.removeItem('token')
-                navigate('/signin')
-            }} className=' text-lg flex items-center rounded-md px-2 bg-red-700 cursor-pointer'>
+            <div onClick={handleLogOut}
+                className=' text-lg flex items-center rounded-md px-2 bg-red-700 cursor-pointer'>
                 <p className='sm:block hidden'>Log out</p>
-                <img className='sm:w-10 w-8' src='logout.svg'/>
-                </div>
+                <img className='sm:w-10 w-8' src='logout.svg' />
+            </div>
         </div>
     )
 }
